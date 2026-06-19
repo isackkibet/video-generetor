e1297d3df8b73d9bdebec154b1ef3f96956853fa
Dancan1907 <dancankalerwa@gmail.com>
2026-06-19 13:04:27 +0300

    Add ModerationModule with controller and service

---
+++ backend/moderation-service/src/moderation.module.ts
@@ -0,0 +1,9 @@
+import { Module } from "@nestjs/common";
+import { ModerationController } from "./moderation.controller";
+import { ModerationService } from "./moderation.service";
+import { PrismaService } from "../../shared/prisma.service";
+@Module({
+  controllers: [ModerationController],
+  providers: [ModerationService, PrismaService],
+})
+export class ModerationModule {}
